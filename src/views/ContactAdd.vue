e2815731d0b3d16afd3154369561e7b6964156b3
unknown <linhb170891@student.ctu.edu.vn>
2022-04-05 19:04:57 +0700

    hoan thanh

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,23 @@
+<template>
+      <div  class="page">
+        <h4>Thêm Liên hệ</h4>
+        <FormAdd
+           
+        />
+      
+    </div>
+</template>
+
+<script>
+import FormAdd from "@/components/FormAdd.vue";
+
+
+export default {
+    components: {
+        FormAdd,
+    },
+   
+    
+    
+};
+</script>
